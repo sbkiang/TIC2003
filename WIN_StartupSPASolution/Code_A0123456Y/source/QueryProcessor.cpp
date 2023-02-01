719f148911f0052c1dd2208b6c65aa8381a35b7b
sbkiang <77959438+sbkiang@users.noreply.github.com>
2023-02-01 18:55:24 +0800

    Update QueryProcessor.cpp

--- WIN_StartupSPASolution/Code_A0123456Y/source/QueryProcessor.cpp
+++ WIN_StartupSPASolution/Code_A0123456Y/source/QueryProcessor.cpp
@@ -24,7 +24,7 @@ void QueryProcessor::evaluate(string query, vector<string>& output) {
 	// check what type of synonym is being declared
 	vector<string> synonymType; 
 
-	int selectIndex, suchThatIndex, patternIndex;
+	int selectIndex = 0, suchThatIndex = 0, patternIndex = 0;
 
 	// check for Next, Parent, Uses, Modifies, Call
 	string designAbstraction;
@@ -36,8 +36,7 @@ void QueryProcessor::evaluate(string query, vector<string>& output) {
 		if (tokens[i] == "procedure" || tokens[i] == "variable" || tokens[i] == "constant" || tokens[i] == "call" || tokens[i] == "assign" || tokens[i] == "stmt" || tokens[i] == "read" || tokens[i] == "print" || tokens[i] == "while" || tokens[i] == "if") {
 			synonymType.push_back(tokens[i]);
 		}
-		
-		
+			
 		if (tokens[i] == "Select") {
 			selectIndex = i;
 		}
@@ -57,31 +56,52 @@ void QueryProcessor::evaluate(string query, vector<string>& output) {
 	// This logic is highly simplified based on iteration 1 requirements and 
 	// the assumption that the queries are valid.
 	
-	if (selectIndex > 0) {
-
-		//Single Select Cause
-		if (synonymType.size() == 1) {
-			if (synonymType[0] == "procedure") {
-				Database::getProcedures(databaseResults);
-			}
-
-			else if (synonymType[0] == "variable") {
-				Database::getVariable(databaseResults);
-			}
-
-			else if (synonymType[0] == "assign" || synonymType[0] == "print" || synonymType[0] == "read" || synonymType[0] == "stmt") {
-				Database::getStmt(synonymType[0], databaseResults);
-			}
-
-			else if (synonymType[0] == "constant") {
-				Database::getConstant(databaseResults);
-			}
+	if (!suchThatIndex && !patternIndex) {
+		if (synonymType[0] == "procedure") {
+			Database::getProcedures(databaseResults);
 		}
 
-		//Multiple Select Cause
-		else {
-			
+		else if (synonymType[0] == "variable") {
+			Database::getVariable(databaseResults);
 		}
+
+		else if (synonymType[0] == "assign" || synonymType[0] == "print" || synonymType[0] == "read" || synonymType[0] == "stmt") {
+			Database::getStmt(synonymType[0], databaseResults);
+		}
+
+		else if (synonymType[0] == "constant") {
+			Database::getConstant(databaseResults);
+		}
+	}
+
+	else if (suchThatIndex) {
+		if (designAbstraction == "Parent") { 
+			//Relationship between statement
+		}
+		else if (designAbstraction == "ParentT") { //parentT = parent*
+			//Relationship between statement
+		}
+		else if (designAbstraction == "Next") {
+			//Relationship between statement
+		}
+		else if (designAbstraction == "NextT") { //nextT = next* 
+			//Relationship between statement
+		}
+		else if (designAbstraction == "Calls") {
+			//Relationship between procedure
+		}
+		else if (designAbstraction == "Calls") { //callT = call* 
+			//Relationship between procedure
+		}
+		else if (designAbstraction == "Use") { 
+			//Relationship between statement/procedure and variable
+		}
+		else if (designAbstraction == "Modifies") {
+			//Relationship between statement/procedure and variable
+		}
+	}
+	else if (patternIndex) {
+
 	}
 	
 	// post process the results to fill in the output vector
