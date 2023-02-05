162e895c91459fb8f7df69cb97ca6d0292b3b260
sbkiang <77959438+sbkiang@users.noreply.github.com>
2023-02-05 19:58:12 +0800

    query

--- WIN_StartupSPASolution/Code_18/source/QueryProcessor.cpp
+++ WIN_StartupSPASolution/Code_18/source/QueryProcessor.cpp
@@ -1,6 +1,9 @@
 #include "QueryProcessor.h"
 #include "Tokenizer.h"
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
 
 // constructor
 QueryProcessor::QueryProcessor() {}
@@ -27,13 +30,19 @@ void QueryProcessor::evaluate(string query, vector<string>& output) {
 	int selectIndex = 0, suchThatIndex = 0, patternIndex = 0;
 
 	// check for Next, Parent, Uses, Modifies, Call
-	string designAbstraction;
+	string designAbstraction = "";
 
+	string firstStrIdx = "", secondStrIdx = "";
+	int firstIntIdx = 0, secondIntIdx = 0;
+	bool bracket = false, isNumber = true;
 
 	for (int i = 0; i < tokens.size(); i++) {
 
 		//check what type of synonym is
 		if (tokens[i] == "procedure" || tokens[i] == "variable" || tokens[i] == "constant" || tokens[i] == "call" || tokens[i] == "assign" || tokens[i] == "stmt" || tokens[i] == "read" || tokens[i] == "print" || tokens[i] == "while" || tokens[i] == "if") {
+			//uppercase to lowercase
+			transform(tokens[i].begin(), tokens[i].end(), tokens[i].begin(), [](unsigned char c) { return std::tolower(c); });
+			
 			synonymType.push_back(tokens[i]);
 		}
 
@@ -43,14 +52,73 @@ void QueryProcessor::evaluate(string query, vector<string>& output) {
 		else if (tokens[i] == "such" && tokens[i + 1] == "that") {
 			suchThatIndex = i;
 			designAbstraction = tokens[i + 2];
+
+			//uppercase to lowercase
+			transform(designAbstraction.begin(), designAbstraction.end(), designAbstraction.begin(), [](unsigned char c) { return std::tolower(c); });
+
+			if (tokens[i + 3] == "*") {
+				if (designAbstraction == "parent") {
+					designAbstraction = designAbstraction + "T";
+				}
+				else if (designAbstraction == "calls") {
+					designAbstraction = designAbstraction + "T";
+				}
+				else if (designAbstraction == "next") {
+					designAbstraction = designAbstraction + "T";
+				}
+			}
+
+			while (tokens[i] != ")") {
+				if(tokens[i] == "(" || bracket) {			
+					if (tokens[i] == ",") {
+						cout << "11";
+						for (char c : tokens[i+1]) {
+							if (!isdigit(c)) {
+								cout << "55";
+								isNumber = false;
+								break;
+							}
+						}
+
+						if (isNumber) {
+							cout << "22";
+							secondIntIdx = stoi(tokens[i+1]); //convert string to int
+						}
+						else 
+							cout << "33";
+							secondStrIdx = tokens[i+1];
+					}
+					else {
+						for (char c : tokens[i]) {
+							if (!isdigit(c)) {
+								isNumber = false;
+								break;
+							}
+						}
+						if (isNumber)
+							firstIntIdx = stoi(tokens[i]); //convert string to int
+						else
+							firstStrIdx = tokens[i];
+					}
+					bracket = true;
+				}
+				i++;
+			}
+
 		}
 		else if (tokens[i] == "pattern") {
 			patternIndex = i;
 		}
+
+	 
+		
+
 	}
 
 	// create a vector for storing the results from database
-	vector<string> databaseResults;
+	vector<string> databaseResults, synonymResults;
+
+	
 
 	// call the method in database to retrieve the results
 	// This logic is highly simplified based on iteration 1 requirements and 
@@ -73,30 +141,34 @@ void QueryProcessor::evaluate(string query, vector<string>& output) {
 			Database::getConstant(databaseResults);
 		}
 	}
-
 	else if (suchThatIndex) {
-		if (designAbstraction == "Parent") {
+		if (designAbstraction == "parent") {
+			//Relationship between statement
+			if(synonymType[0] == "assign" || synonymType[0] == "print" || synonymType[0] == "read" || synonymType[0] == "stmt" || synonymType[0] == "call") {
+				Database::getStmt(synonymType[0], synonymResults);
+				cout << firstStrIdx << " @@ " << secondIntIdx;
+;
+			}
+		}
+		else if (designAbstraction == "parentT") { //parentT = parent*
 			//Relationship between statement
 		}
-		else if (designAbstraction == "ParentT") { //parentT = parent*
+		else if (designAbstraction == "next") {
 			//Relationship between statement
 		}
-		else if (designAbstraction == "Next") {
+		else if (designAbstraction == "nextT") { //nextT = next* 
 			//Relationship between statement
 		}
-		else if (designAbstraction == "NextT") { //nextT = next* 
-			//Relationship between statement
-		}
-		else if (designAbstraction == "Calls") {
+		else if (designAbstraction == "calls") {
 			//Relationship between procedure
 		}
-		else if (designAbstraction == "Calls") { //callT = call* 
+		else if (designAbstraction == "callsT") { //callT = call* 
 			//Relationship between procedure
 		}
-		else if (designAbstraction == "Use") {
+		else if (designAbstraction == "use") {
 			//Relationship between statement/procedure and variable
 		}
-		else if (designAbstraction == "Modifies") {
+		else if (designAbstraction == "modifies") {
 			//Relationship between statement/procedure and variable
 		}
 	}
