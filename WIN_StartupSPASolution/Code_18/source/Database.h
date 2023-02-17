239335ec6926f0bf51e9d638301ca116d6a694c3
LeeTheGreat <weeee148@hotmail.com>
2023-02-17 23:26:18 +0800

    Add use

--- WIN_StartupSPASolution/Code_18/source/Database.h
+++ WIN_StartupSPASolution/Code_18/source/Database.h
@@ -17,48 +17,33 @@ public:
 	// method to close the database connection
 	static void close();
 
-	// method to insert/get a procedure into the database
 	static void insertProcedure(string procedureName);
 	static void getProcedures(vector<string>& results);
 
-	// method to insert/get a statement into the database
 	static void insertStatement(int statementNumber, string statementName, string statementType, string text);
 	static void getStatement(string type, vector<string>& results);
 	static void getStmt(string type, vector<string>& results);
 
-	// method to insert/get a variable into the database
 	static void insertVariable(string variablename, int statementNumber);
 	static void getVariable(vector<string>& results);
 
-	// method to insert/get a constant into the database
+	// method to insert a constant into the database
 	static void insertConstant(string value);
 	static void getConstant(vector<string>& results);
 
-	// method to insert/get a parent into the database
+	// method to insert a parent into the database
 	static void insertParent(int parentStmt, int childStart, int childEnd);
 	static void getParent(string stmtNum1, string stmtNum2, vector<string>& results);
 	static void getChildren(string stmtNum1, string stmtNum2, string statementType, vector<string>& results);
 	static void getParentChildren(bool findparent, string resultType, string filterType, vector<string>& results);
 	
-<<<<<<< Updated upstream
-
-	// method to insert/get a modify into the database
-=======
 	// method to insert a modify into the database
->>>>>>> Stashed changes
 	static void insertModifies(int statementNumber, string procedureName, string variablename);
 
-	// method to insert/get a use into the database
+	// method to insert a use into the database
 	static void insertUse(int statementNumber, string procedureName, string variablename);
-<<<<<<< Updated upstream
-	static void getUseStmt(string stmtNum1, string stmtNum2, bool lhs, vector<string>& results);
-	static void getUseEnt(string stmtNum1, string stmtNum2, vector<string>& results);
-
-	// method to insert/get a next into the database
-=======
 	static void getUse(string entity, string variable, vector<string>& results); // for "assign a" and "print pn"
 
->>>>>>> Stashed changes
 	static void insertNext(int stmtNum1, int stmtNum2);
 	static void getNext(int stmtNum1, int stmtNum2, vector<string>& results);
 	static void getNext_T(int stmtNum1, int stmtNum2, vector<string>& results);
