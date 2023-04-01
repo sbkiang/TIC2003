4960c1afa4585c131e5e1247a118c57b5e6bb17d
sbkiang <77959438+sbkiang@users.noreply.github.com>
2023-04-01 21:45:57 +0800

    update ClauseCall

--- WIN_StartupSPASolution/Code_18/SPA/ClauseCall.cpp
+++ WIN_StartupSPASolution/Code_18/SPA/ClauseCall.cpp
@@ -2,10 +2,10 @@
 #include "ClauseCall.h"
 
 // Call(procedure, procedure)
-string Call::GetCallConstruct_Synonym_Synonym()
+string Call::GetCallConstruct_Synonym_Synonym(string input1)
 {
 	char sql[100] = {};
-	sprintf_s(sql, "select c.line_num");
+	sprintf_s(sql, "select c.line_num as %s", input1.c_str());
 	return string(sql);
 }
 
@@ -17,19 +17,27 @@ string Call::GetCallConstruct_Synonym_NotSynonym(string input1)
 	return string(sql);
 }
 
-// Parent(10, (stmt|read|print|assign|while|if|call))
+// Call("First"/_, procedure)
 string Call::GetCallConstruct_NotSynonym_Synonym(string input2)
 {
 	char sql[100] = {};
-	//sprintf_s(sql, "select p.line_num, s.line_num as %s", input2.c_str());
-	sprintf_s(sql, "select s.line_num as %s", input2.c_str());
+	sprintf_s(sql, "select c.line_num as %s", input2.c_str());
 	return string(sql);
 }
 
-// Parent(10, 12)
+// Call("First"/_, "Second"/_)
 string Call::GetCallConstruct_NotSynonym_NotSynonym()
 {
 	char sql[100] = {};
-	sprintf_s(sql, "select p.line_num, s.line_num");
+	sprintf_s(sql, "select c.line_num, c.line_num");
 	return string(sql);
 }
+
+// Call(_, procedure)
+string Call::GetCall_Any_Synonym(string frontSql, string input2) {
+	char sqlBuf[512] = {};
+	sprintf_s(sqlBuf, " %s from call c where c.variable_name = '%s'", frontSql.c_str(), input2.c_str());
+	return string(sqlBuf);
+}
+
+
